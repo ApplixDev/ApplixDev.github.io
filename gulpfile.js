a537dae552626e4d654b02756faa0eb145bef87b
lyamalou <yazid_amalou@outlook.com>
2017-09-01 20:01:04 +0200

    fix(gulpfile.js): prevent gulp clean task to delete dist/.git folder

--- gulpfile.js
+++ gulpfile.js
@@ -86,7 +86,7 @@ gulp.task('extras', () => {
   }).pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task('clean', del.bind(null, ['.tmp', 'dist/**/*' ,'!dist/.git']));
 
 gulp.task('serve', () => {
   runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {
