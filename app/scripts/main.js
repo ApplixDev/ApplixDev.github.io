46c82837f87d86318f10c1feb8e01582bbc5f796
lyamalou <yazid_amalou@outlook.com>
2017-09-10 00:02:45 +0200

    website enhancement

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,53 +1,32 @@
 $(document).ready(function () {
-    
-    $(window).on('load', function () {
-        if (window.location.hash == '#app') {
-            $('.navbar-fixed, .page-footer').hide();
-        }
-    });
 
     $('.pushpin-nav').pushpin({
-        top: 48
+        top: 58,
+        bottom: 0
     });
 
-    $('.button-collapse').sideNav({ 
-        menuWidth: 240 
+    // Make announcement animate down and display none
+    $('#close').click(function () {
+        $('#announcement').fadeOut(100);
+    });
+    
+    $('.button-collapse').sideNav({
+        menuWidth: 240
     });
 
     // Go to top
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('#scrollup').fadeIn(100);
         } else {
             $('#scrollup').fadeOut(300);
         }
     });
-    
-    $('#scrollup').click(function() {
+
+    $('#scrollup').click(function () {
         $('body,html').animate({
             scrollTop: 0
         }, 500);
     });
 
-    // $(window).resize(function () {
-    //     $('.card-empty').each(function () {
-    //         var parent = $(this).parent('.col');
-    //         var prev = parent.prev('.col');
-    //         var height = false;
-
-    //         if (prev.length < 1) {
-    //             prev = parent.next('.col');
-
-    //             if (prev.length < 1) {
-    //                 height = $(parent).width();
-    //             }
-    //         }
-
-    //         if (!height) {
-    //             height = prev.height();
-    //         }
-
-    //         $(this).height (height);
-    //     });
-    // });
 });
